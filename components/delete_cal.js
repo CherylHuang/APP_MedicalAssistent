627aac9a8484cddf78759c1f48cd60b5bcca4efb
Lai-YingRu <lai860423@gmail.com>
2017-05-12 00:14:45 -0500

    Add files via upload

---
+++ components/delete_cal.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { View, TouchableOpacity } from 'react-native';
+import { Icon } from 'react-native-elements';
+
+class delete_add_icon extends Component {
+
+render(){    
+        const { right, icon } = styles;
+return (
+    <View style={[right]}>
+            <Icon name='delete' size={30} color="#517fa4"
+             containerStyle={{marginRight:5}}
+            onPress={() => {}} 
+            />
+            <Icon name='date-range' size={30} color="#517fa4"
+             containerStyle={{marginRight:5}}
+            onPress={() => {}}
+            />
+    </View>
+);}
+
+}
+
+const styles = {
+    right: {
+        flexDirection:'row'
+    }
+};
+
+export default delete_add_icon;
