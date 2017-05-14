43f06e4efdbb2e6ea80eb348f8fc08ef468c7fd2
Lai-YingRu <lai860423@gmail.com>
2017-05-14 09:33:55 -0500

    Add files via upload

--- pages/EditPassword.js
+++ pages/EditPassword.js
@@ -1,19 +1,105 @@
 import React, { Component } from 'react';
-import { Text, View, Image } from 'react-native';
-import { Icon } from 'react-native-elements';
+import { Text, View, Image,Alert} from 'react-native';
+import { Icon,FormInput,FormLabel,Button } from 'react-native-elements';
 //import {Password} from 'react-native-input-password';
 
-
+const password=null;
+const confirm=null;
 class EditPassword extends Component {
+   state = {
+  password:null,
+  confirm:null,
+}
+
+  onSignIn=() =>{
+
+    if((password)==(confirm)){
+       Alert.alert(
+            '密碼設定成功!',
+            null,
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed!')},
+            ],
+            {
+              cancelable: false
+            }
+          )
+
+    }
+    else{
+      Alert.alert(
+            '確認密碼有誤!',
+            null,
+            [
+              {text: 'OK', onPress: () => console.log('OK Pressed!')},
+            ],
+            {
+              cancelable: false
+            }
+          )
+    }
+  }
+
+
+ 
   render() {
+    const { state,psaaword } = styles;
     return(
-         <View>
-           {/*<Password maxLength={4}></Password>*/}
-         </View>
+        <View style={{flex:1,marginTop:20}}>
+            {/*<Password maxLength={4}></Password>*/}
+          <FormLabel >請輸入4位密碼</FormLabel>
+          <View style={{flexDirection:'row'}}>  
+          <FormInput
+            containerStyle={{flex:1,width:200,height:50,backgroundColor:'white'}}
+            inputStyle={{fontSize:30}}
+            secureTextEntry
+            autoCorrect={false}
+            autoCapitalize='none'
+            placeholder='password'
+            value={password}
+            maxLength={4}
+            onChangeText={password => this.setState({ password })}
+            
+          />
+          </View>
+          <FormLabel >確認密碼</FormLabel>
+          <View style={{flexDirection:'row'}}>
+          <FormInput
+            containerStyle={{flex:1,height:50,backgroundColor:'white'}}
+            inputStyle={{fontSize:30}}
+            secureTextEntry
+            autoCorrect={false}
+            autoCapitalize='none'
+            placeholder='password'
+            value={confirm}
+            maxLength={4}
+            onChangeText={confirm => this.setState({ confirm })}
+            
+          />
+          </View>
+          <Button
+          title='Sign in'
+          backgroundColor='#4AAF4C'
+          onPress={this.onSignIn}
+          buttonStyle={{marginTop:50,borderRadius:5}}
+          backgroundColor='#517fa4' 
+          textStyle={{fontSize:18,color:"white"}}
+          />
+
+        </View>
+
 
          );
      }    
+      
  }
 
+const styles={
+  state:{
+    fontSize:35
+
+  },
+
+}
 
 export default EditPassword;
